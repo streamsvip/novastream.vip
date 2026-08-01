77becb62a8a5a25069482ec3eb6071877a446267
impulso project <velithaz@gmail.com>
2026-08-01 15:54:59 -0500

    Update novastream.js

--- novastream.js
+++ novastream.js
@@ -2,9 +2,10 @@
    CONFIG
 ========================= */
 
-const MC_FB_CONFIG = {
+const firebaseConfig = {
   apiKey: "AIzaSyCwMr1Ie2DmAePzI0X4qsSR5jE70OKbRkA",
   authDomain: "novastream-f3e15.firebaseapp.com",
+  databaseURL: "https://novastream-f3e15-default-rtdb.firebaseio.com",
   projectId: "novastream-f3e15",
   storageBucket: "novastream-f3e15.firebasestorage.app",
   messagingSenderId: "356156093772",
